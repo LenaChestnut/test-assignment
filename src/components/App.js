a44b03630b9ba6b627f8975e89b9d0b7982732aa
Elena Kashtanova <lenachestnut@gmail.com>
2020-11-22 15:22:57 +0300

    Add title

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-    return <div></div>;
+    return (
+        <div className="app">
+            <h1>Ты сегодня покормил кота?</h1>
+        </div>
+    );
 }
 
 export default App;
