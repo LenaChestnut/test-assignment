e601d34f9a282746ff19888fe16fb66ba612ad53
Elena Kashtanova <lenachestnut@gmail.com>
2020-11-23 13:09:00 +0300

    Set up Card component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
 import './App.css';
+import Card from './Card/Card';
 
 function App() {
     return (
         <div className="app">
             <Header />
+            <Card />
         </div>
     );
 }
