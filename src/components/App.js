f0a58d15a2cdbfe78ae1894cf1b4ad95cdbeda36
Elena Kashtanova <lenachestnut@gmail.com>
2020-11-25 19:21:01 +0300

    Display three cards

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,16 @@ function App() {
                     flavorDescription={product.flavors['foie-gras'].flavorDescription}
                     weight={product.weight[0]}
                 />
+                <Card
+                    flavorName={product.flavors.fish.flavorName}
+                    flavorDescription={product.flavors.fish.flavorDescription}
+                    weight={product.weight[1]}
+                />
+                <Card
+                    flavorName={product.flavors.chicken.flavorName}
+                    flavorDescription={product.flavors.chicken.flavorDescription}
+                    weight={product.weight[2]}
+                />
             </div>
         </div>
     );
